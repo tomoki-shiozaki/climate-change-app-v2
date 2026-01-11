3aec608bb7abc91e07bbdebafffc87158d0909f3
shiozaki <shiozaki.tomoki2@gmail.com>
2026-01-11 16:35:28 +0900

    chore: add terraform files

--- infra/artifact_registry.tf
+++ infra/artifact_registry.tf
@@ -1,5 +1,5 @@
-resource "google_artifact_registry_repository" "climate_repo" {
-  repository_id = "climate-change-app"
+resource "google_artifact_registry_repository" "climate_v2_repo" {
+  repository_id = "climate-change-app-v2"
   location      = var.region
   format        = "DOCKER"
   description   = "Docker repo for climate change app"
