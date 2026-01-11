a13aa0b70054bf69da292a8268a43b7f1a3e3b96
shiozaki <shiozaki.tomoki2@gmail.com>
2026-01-11 17:32:52 +0900

    chore: add cloud-run-deploy.yml

--- infra/cloud_build.tf
+++ infra/cloud_build.tf
@@ -9,4 +9,13 @@ resource "google_cloudbuild_trigger" "app_trigger" {
       branch = "^main$"
     }
   }
+
+  # GitHub Actions から渡す秘密情報を Terraform 変数経由で Substitution に渡す
+  substitutions = {
+    _SECRET_KEY   = var.secret_key
+    _DATABASE_URL = var.database_url
+    _ALLOWED_HOSTS = var.allowed_hosts
+    _CSRF_TRUSTED_ORIGINS = var.csrf_trusted_origins
+    _CORS_ORIGIN_WHITELIST = var.cors_origin_whitelist
+  }
 }
