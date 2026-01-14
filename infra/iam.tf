a068e8ce02876cf1d055314fe0c3a9fab488f303
shiozaki <shiozaki.tomoki2@gmail.com>
2026-01-14 10:05:34 +0900

    chore: add terraform files

--- infra/iam.tf
+++ infra/iam.tf
@@ -12,3 +12,15 @@ resource "google_project_iam_member" "cloudbuild_artifact_registry" {
   role    = "roles/artifactregistry.writer"
   member  = "serviceAccount:${local.cloudbuild_sa}"
 }
+
+resource "google_project_iam_member" "cloudbuild_cloudrun" {
+  project = var.project_id
+  role    = "roles/run.admin"
+  member  = "serviceAccount:${local.cloudbuild_sa}"
+}
+
+resource "google_project_iam_member" "cloudbuild_sa_user" {
+  project = var.project_id
+  role    = "roles/iam.serviceAccountUser"
+  member  = "serviceAccount:${local.cloudbuild_sa}"
+}
