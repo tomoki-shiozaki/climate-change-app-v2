39193836dc2ba841c2110dd5e39b851233d433c1
shiozaki <shiozaki.tomoki2@gmail.com>
2026-01-15 18:24:25 +0900

    chore: add terraform files

--- infra/cloudbuild_github.tf
+++ infra/cloudbuild_github.tf
@@ -1,21 +1,17 @@
 ############################################
-# Cloud Build GitHub Connection (v2)
+# 既存の Cloud Build GitHub Connection を参照
 ############################################
-resource "google_cloudbuildv2_connection" "github" {
-  location = "asia-northeast1"
+data "google_cloudbuildv2_connection" "github" {
   name     = "github-connection-climate-v2"
-
-  github_config {
-    app_installation_id = var.github_app_installation_id
-  }
+  location = "asia-northeast1"
 }
 
 ############################################
 # Cloud Build GitHub Repository (v2)
 ############################################
 resource "google_cloudbuildv2_repository" "repo" {
-  location           = "global"
-  name               = var.github_repo
-  parent_connection  = google_cloudbuildv2_connection.github.id
-  remote_uri         = "https://github.com/${var.github_owner}/${var.github_repo}.git"
+  location          = "global"
+  name              = var.github_repo
+  parent_connection = data.google_cloudbuildv2_connection.github.id
+  remote_uri        = "https://github.com/${var.github_owner}/${var.github_repo}.git"
 }
