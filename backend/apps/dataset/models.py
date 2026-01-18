1584501d60eac757bc9e0f165331d6089b70032b
shiozaki <shiozaki.tomoki2@gmail.com>
2026-01-18 13:59:17 +0900

    feat(dataset): add Dataset model

--- backend/apps/dataset/models.py
+++ backend/apps/dataset/models.py
@@ -1,3 +1,27 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Dataset(models.Model):
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="datasets",
+    )
+    name = models.CharField(max_length=255)
+
+    source_file = models.FileField(
+        upload_to="datasets/source/",
+    )
+
+    status = models.CharField(
+        max_length=20,
+        choices=[
+            ("uploaded", "Uploaded"),
+            ("parsed", "Parsed"),
+            ("failed", "Failed"),
+        ],
+        default="uploaded",
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
