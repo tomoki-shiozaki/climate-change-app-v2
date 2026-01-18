3a0b89aa448f64f0c2a9bfc05338822ee0f49719
shiozaki <shiozaki.tomoki2@gmail.com>
2026-01-18 15:19:29 +0900

    feat(dataset): add dataset link to Homepage

--- backend/apps/api/dataset/urls.py
+++ backend/apps/api/dataset/urls.py
@@ -7,7 +7,7 @@ from apps.api.dataset.views import DatasetUploadAPIView
 app_name = "dataset"
 
 urlpatterns = [
-    path("", DatasetUploadAPIView.as_view(), name="upload"),
+    path("upload/", DatasetUploadAPIView.as_view(), name="upload"),
 ]
 
 if settings.DEBUG:
