363fe2c238bfb710fc58e47caa61a81375dcacc4
shiozaki <shiozaki.tomoki2@gmail.com>
2026-01-18 17:39:32 +0900

    fix(dataset): fix media files url

--- backend/apps/api/dataset/urls.py
+++ backend/apps/api/dataset/urls.py
@@ -1,5 +1,3 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.urls import path
 
 from apps.api.dataset.views import DatasetUploadAPIView
@@ -9,6 +7,3 @@ app_name = "dataset"
 urlpatterns = [
     path("upload/", DatasetUploadAPIView.as_view(), name="upload"),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
