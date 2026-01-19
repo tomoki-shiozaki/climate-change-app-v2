a48a7914550604668587f8f838294a6f75829d72
shiozaki <shiozaki.tomoki2@gmail.com>
2026-01-19 18:36:11 +0900

    feat(dataset): add schema field to DatasetSerializer

--- backend/apps/api/dataset/serializers.py
+++ backend/apps/api/dataset/serializers.py
@@ -8,10 +8,19 @@ class DatasetSerializer(serializers.ModelSerializer):
     owner = serializers.PrimaryKeyRelatedField(read_only=True)
     status = serializers.CharField(read_only=True)
     created_at = serializers.DateTimeField(read_only=True)
+    schema = serializers.JSONField(required=False)  # フロントから送信可能
 
     class Meta:
         model = Dataset
-        fields = ["id", "name", "source_file", "owner", "status", "created_at"]
+        fields = [
+            "id",
+            "name",
+            "source_file",
+            "owner",
+            "status",
+            "schema",
+            "created_at",
+        ]
 
     def create(self, validated_data):
         # リクエストのユーザーを owner にセット
