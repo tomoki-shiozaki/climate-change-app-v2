8564a5e89e48fa4507190d2c157a1ec5dabf3db7
shiozaki <shiozaki.tomoki2@gmail.com>
2026-01-25 16:01:10 +0900

    feat(DatasetList): add pagination

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -106,6 +106,8 @@ REST_FRAMEWORK = {
     "DEFAULT_RENDERER_CLASSES": [
         "rest_framework.renderers.JSONRenderer",
     ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    "PAGE_SIZE": 20,  # 1ページあたりの件数
 }
 
 # 開発環境のみ Browsable API と SessionAuth を追加
