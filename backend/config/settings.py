2b75834a046d7fb40f41a3e00c1c173e09b8cf5b
shiozaki <shiozaki.tomoki2@gmail.com>
2026-01-25 20:39:00 +0900

    chore(pagination): configure pagination setting

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -107,8 +107,7 @@ REST_FRAMEWORK = {
     "DEFAULT_RENDERER_CLASSES": [
         "rest_framework.renderers.JSONRenderer",
     ],
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
-    "PAGE_SIZE": 10,  # 1ページあたりの件数
+    "DEFAULT_PAGINATION_CLASS": "apps.core.pagination.StandardPagination",
 }
 
 # 開発環境のみ Browsable API と SessionAuth を追加
